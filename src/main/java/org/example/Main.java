669347a01674ad9bdfeedfbc730449cef0ac0b2a
Aiden <yumenushii.dev@gmail.com>
2025-02-02 02:29:34 +0100

    Finalized

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        NoGenericMethods obj1 = new NoGenericMethods("A", "B", "C");
+        NoGenericMethods obj2 = new NoGenericMethods("C", "A", "B");
+        NoGenericMethods obj3 = new NoGenericMethods("B", "C", "A");
+
+        obj1.showValues();
+        obj2.showValues();
+        obj3.showValues();
+    }
+}
